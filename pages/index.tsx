b2d4c4f329a193873331000bab777caa4bdfa54d
B8N4S8N <79737258+B8N4S8N@users.noreply.github.com>
2023-01-23 20:18:48 -0800

    change page text

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,14 @@ function Home() {
   return (
     <Page className="flex flex-col gap-12">
       <section className="flex flex-col gap-6">
-        <Text variant="h1">OpenAI GPT-3 text model usage example</Text>
+        <Text variant="h1">Welcome to a new era of recovery support</Text>
         <Text className="text-zinc-600">
-          In this example, a simple chat bot is implemented using Next.js, API
-          Routes, and OpenAI API.
+          S.A.R.A. is Serenium's AI Recovery Assistant. She was built by OSR to aid those in need of support. In any capacity. She is recovery focused, but capable of of so much more. Go ahead and ask her anything, and start owning your path.
         </Text>
       </section>
 
       <section className="flex flex-col gap-3">
-        <Text variant="h2">AI Chat Bot:</Text>
+        <Text variant="h2">Chat with S.A.R.A:</Text>
         <div className="lg:w-2/3">
           <Chat />
         </div>

73b0c39c251d849298458fef096bd0ec7b8c8812
B8N4S8N <79737258+B8N4S8N@users.noreply.github.com>
2023-01-23 20:25:43 -0800

    maybe

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ function Home() {
       <section className="flex flex-col gap-6">
         <Text variant="h1">Welcome to a new era of recovery support</Text>
         <Text className="text-zinc-600">
-          S.A.R.A. is Serenium's AI Recovery Assistant. She was built by OSR to aid those in need of support. In any capacity. She is recovery focused, but capable of of so much more. Go ahead and ask her anything, and start owning your path.
+          S.A.R.A. is The SerenityChain AI Recovery Assistant. She was built by OSR to aid those in need of support. In any capacity. She is recovery focused, but capable of of so much more. Go ahead and ask her anything, and start owning your path.
         </Text>
       </section>
 

a16dad36051c18b2f7243cabf27a3085787b0907
B8N4S8N <79737258+B8N4S8N@users.noreply.github.com>
2023-02-05 02:59:19 +0000

    More UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,17 @@ import { Chat } from '../components/Chat'
 
 function Home() {
   return (
-    <Page className="flex flex-col align-middle text-center gap-12">
-      <section className="flex flex-col gap-6">
+    <Page className="flex flex-col align-middle gap-12">
+      <section className="flex flex-col text-center gap-6">
         <Text variant="h1">Welcome to a new era of recovery support</Text>
-        <Text className="text-zinc-600">
-          S.A.R.A. is The SerenityChain AI Recovery Assistant. She was built by OSR to aid those in need of support. In any capacity. She is recovery focused, but capable of of so much more. Go ahead and ask her anything, and start owning your path.
+        <Text className="text-zinc-600 text-center ">
+          S.A.R.A. is Serenim`s AI Recovery Assistant. She was built by OSR to aid those in need of support. In any capacity. She is recovery focused, but capable of so much more. Go ahead and ask her anything, and start owning your path.
         </Text>
       </section>
 
-      <section className="flex align-middle flex-col gap-3">
+      <section className="flex flex-col gap-3">
         <Text variant="h2">Chat with S.A.R.A:</Text>
-        <div className="lg:w-2/3 align-middle">
+        <div className="lg:w-4/4">
           <Chat />
         </div>
       </section>
